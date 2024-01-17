c8dc892504d0da7ff5fbf75ff5e17439cd826033
Marcos <marcosugaldemartinez@gmail.com>
2024-01-17 14:03:10 +0100

    fix: fixed bug in code that broke app before starting

--- backend-ecommerce/models/auth/queries.js
+++ backend-ecommerce/models/auth/queries.js
@@ -1,13 +1,13 @@
-const { sql } = require('slonik')
+const { sql } = require("slonik");
 
-const insertUser = (email, username, password, admin) = sql.unsafe`
+const insertUser = (email, username, password, admin) => sql.unsafe`
     INSERT INTO users (
         email, username, password, admin
     ) VALUES (
         ${email}, ${username}, ${password}, ${admin}
     )
-`
+`;
 
 module.exports = {
-    insertUser
-}
+  insertUser,
+};
