43568bdbb73a3d81da823dd00ca21fd513c960d5
Marcos <marcosugaldemartinez@gmail.com>
2024-01-17 15:02:06 +0100

    feat: successfully created route to loging in

--- backend-ecommerce/routes/auth.js
+++ backend-ecommerce/routes/auth.js
@@ -4,9 +4,11 @@ const { authorizer, fieldsValidator } = require("../midlewares");
 const authControllers = require("../controllers/auth");
 
 const signupValidator = fieldsValidator("email", "username", "password");
+const signinValidator = fieldsValidator("email", "password");
 
 module.exports = (db) => {
   router.post("/signup", signupValidator, authControllers.signup(db));
+  router.post("/signin", signinValidator, authControllers.signin(db));
 
   return router;
 };
