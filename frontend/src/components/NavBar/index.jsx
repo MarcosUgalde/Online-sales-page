30afae7975e30383c50c7bdb4e4fc1ee57312e39
Marcos <marcosugaldemartinez@gmail.com>
2024-01-22 19:20:49 +0100

    feat: created nav bar component

--- frontend/src/components/NavBar/index.jsx
+++ frontend/src/components/NavBar/index.jsx
@@ -1,26 +1,47 @@
+import Styled from './styles'
 import { Link, useRoute } from "wouter";
 import { useState } from "react";
+import { useUser, useLogout } from "../../hooks";
 
 const CustomLink = ({ href, children }) => {
     const [isActive] = useRoute(href)
 
     return (
         <Link {... { href }}>
-            <a {...{ href, isActive }}>{children}</a>
+            <Styled.Anchor {...{ href, isActive }}>{children}</Styled.Anchor>
         </Link>
     )
 }
 
 const NavBar = () => {
+    const { data } = useUser()
+    const handleLogout = useLogout()
     const [isDropDownOpen, setIsDropDownOpen] = useState(false)
 
     const toggleDropDown = () => {
         setIsDropDownOpen((prev) => !prev)
     }
 
+    const handleLogoutClick = () => {
+        handleLogout()
+        setIsDropDownOpen(false)
+    }
+
     return (
-        <nav>
-            
-        </nav>
+        <Styled.Nav>
+            <CustomLink href='/'>Main menu</CustomLink>
+            {data && (
+                <Styled.Userlogout>
+                    <Styled.User onClick={toggleDropDown}>{data?.username}</Styled.User>
+                    {isDropDownOpen && (
+                        <Styled.Dropdown>
+                            <li onClick={handleLogoutClick}>Logout</li>
+                        </Styled.Dropdown>
+                    )}
+                </Styled.Userlogout>
+            )}
+        </Styled.Nav>
     )
 }
+
+export default NavBar
