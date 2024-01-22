b374d88f0944c715615c42f2549a8d64122d00f1
Marcos <marcosugaldemartinez@gmail.com>
2024-01-22 20:04:52 +0100

    feat: created basic login form

---
+++ frontend/src/pages/Login/index.jsx
@@ -0,0 +1,18 @@
+const Login = () => {
+    return (
+        <>
+            <h1>Login Page</h1>
+            <form action="">
+                <label htmlFor="">Insert a valid email</label>
+                <input type="text" />
+                <label htmlFor="">Username</label>
+                <input type="text" />
+                <label htmlFor="">Insert password</label>
+                <input type="password" />
+                <input type="submit" />
+            </form>
+        </>
+    )
+}
+
+export default Login
