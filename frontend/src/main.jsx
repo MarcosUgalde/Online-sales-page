4458b10015910143b3f70571638a8275bb41bb9d
Marcos <marcosugaldemartinez@gmail.com>
2024-01-22 20:01:32 +0100

    feat: imported Login page to main file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Route, Switch, Redirect } from 'wouter'
+import Guard from './components/Guard/index.jsx'
+import Provider from './context/Provider.jsx'
 import Landing from './pages/Landing/index..jsx'
+import Login from './pages/Login/index.jsx'
 
 const Main = () => {
   return (
-    <Landing />
+    <Provider>
+      <Switch>
+        <Route path='/' component={Landing}/>
+        <Route path='/login' component={Login} />
+      </Switch>
+    </Provider>
   )
 }
 
