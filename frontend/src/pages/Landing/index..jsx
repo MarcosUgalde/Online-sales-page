f9c1ffa2a550e9ed7e3b98c445ca6359ffaffc14
Marcos <marcosugaldemartinez@gmail.com>
2024-01-24 18:56:08 +0100

    feat: successfully rendered component in landing page

--- frontend/src/pages/Landing/index..jsx
+++ frontend/src/pages/Landing/index..jsx
@@ -1,8 +1,10 @@
 import { useProducts } from "../../hooks"
+import Item from "../../components/Item"
+
 function Landing() {
   
   const { data: response, isLoading } = useProducts()
-  console.log('products: ', response)
+  console.log('response: ', response)
 
   if(isLoading){
     return <p>Loading...</p>
@@ -12,7 +14,17 @@ function Landing() {
     <>
       <h1>Hello word</h1>
       <main>
-
+        {response?.data?.map((product) => {
+          return (
+            <>
+              <ul>
+                <li>
+                  <Item product={product} />
+                </li>
+              </ul>
+            </>
+          )
+        })}
       </main>
     </>
   )
