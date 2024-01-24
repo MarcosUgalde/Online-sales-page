dfa67716f1df905d94a6c4abd0a27be5b11a4b35
Marcos <marcosugaldemartinez@gmail.com>
2024-01-24 12:16:49 +0100

    fix: fixed code error in Login styles file

--- frontend/src/pages/Landing/index..jsx
+++ frontend/src/pages/Landing/index..jsx
@@ -1,9 +1,19 @@
-
+import { useProducts } from "../../hooks"
 function Landing() {
+  
+  const { data: response, isLoading } = useProducts()
+  console.log('products: ', response)
+
+  if(isLoading){
+    return <p>Loading...</p>
+  }
   
   return (
     <>
       <h1>Hello word</h1>
+      <main>
+
+      </main>
     </>
   )
 }
