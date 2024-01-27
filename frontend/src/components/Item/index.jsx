9a9608222f2f0a4cfd4db08963abd996bebf506f
Marcos <marcosugaldemartinez@gmail.com>
2024-01-27 09:58:09 +0100

    feat: successfully added link to the correct Item page

--- frontend/src/components/Item/index.jsx
+++ frontend/src/components/Item/index.jsx
@@ -1,11 +1,13 @@
 import Styled from './styles'
 
 const Item = ({ product }) => {
+    const id = product?.id
+    const url = `/product-info/${id}`
     return (
         <Styled.Item>
             <p>{product?.product_name}</p> 
             <p>{product?.quantity} ud disponibles</p>
-            <a href="">See more</a> 
+            <a href={url}>See more</a> 
         </Styled.Item>
     )
 }
